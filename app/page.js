56d52ccebc33e2107c69de82f2f5ec447f1dd70d
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-03-25 15:33:10 +0500

    image optimization

--- app/page.js
+++ app/page.js
@@ -1,3 +1,7 @@
+// In nextjs, we have Image tag from Nextjs,
+// Which will optimize the image internal images.
+// And if we want to use external image, we have to specify the url in next.config.mjs file.
+
 export default function Home() {
   return (
     <>
