bb4d67d05db4e02a0753c106ad0f5738f9f3f026
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-03-25 15:36:59 +0500

    fonts

--- app/page.js
+++ app/page.js
@@ -1,7 +1,3 @@
-// In nextjs, we have Image tag from Nextjs,
-// Which will optimize the image internal images.
-// And if we want to use external image, we have to specify the url in next.config.mjs file.
-
 export default function Home() {
   return (
     <>
