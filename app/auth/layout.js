f21a866bdf7f470a5060df1d6fae3dba80c9ac0e
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-03-25 14:53:24 +0500

    Conditional Layout

--- app/auth/layout.js
+++ app/auth/layout.js
@@ -1,8 +1,18 @@
+"use client";
+import { usePathname } from "next/navigation";
+
 
 function Auth({children}) {
+    // We can also make layout conditional.
+
+    const pathname = usePathname();
+    console.log(pathname);
+
+
 
     return (
         <>
+         {pathname == '/auth/register' ? <h1> Register</h1> : <h1> Not Register Endpoint </h1>}
 Auth Header
 
         {children}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,3 @@
-// Layout file
-// A layout is like a blueprint that helps you make all the pages of your website.
-// look the same. It's a way to put common things like, header, footer, etc.. that appears on every page.
-// in one place.
-// We can define a layout file in any route for that element.
-// We have to ues children prop to add the content to that layout.
 export default function Home() {
   return (
     <>
